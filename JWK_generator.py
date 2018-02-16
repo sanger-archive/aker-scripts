4056f2c5c12f9508c9ed0a39f0aca7f48dc2d38f
David Robinson <dr6@sanger.ac.uk>
2018-02-16 16:14:07 +0000

    JWT_generator: Don't use eval()

--- JWK_generator.py
+++ JWK_generator.py
@@ -2,7 +2,9 @@
 
 # Generates a string can be used for a JWT key
 import jwcrypto.jwk as jwk
-key = eval(jwk.JWK.generate(kty='oct', size=2048).export())
+from ast import literal_eval
+
+key = literal_eval(jwk.JWK.generate(kty='oct', size=2048).export())
 
 print(key["k"])
 
